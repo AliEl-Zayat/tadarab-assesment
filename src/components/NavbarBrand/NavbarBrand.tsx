efe0df20330a63639879776b4d513905b92181cd
Ali El-Zayat <alielzayat07@gmail.com>
2024-06-25 08:44:54 +0300

    feat:Accessibility title for navbar brand

--- src/components/NavbarBrand/NavbarBrand.tsx
+++ src/components/NavbarBrand/NavbarBrand.tsx
@@ -12,7 +12,10 @@ const NavbarBrand = ({}: Props) => {
         <Image src={NAVBAR_LOGO} alt="Tadarab Logo" width={88.52} height={30} />
       </Link>
       <Divider className="bg-grey opacity-[0.3]" width="2px" height="40px" />
-      <p className="text-[17px] text-[#222222] font-extrabold text-ellipsis line-clamp-1">
+      <p
+        className="text-[17px] text-[#222222] font-extrabold text-ellipsis line-clamp-1 md:text-[14px]"
+        title="دورة التسويق الفعال للمشروعات الصغيرة"
+      >
         دورة التسويق الفعال للمشروعات الصغيرة
       </p>
     </div>
